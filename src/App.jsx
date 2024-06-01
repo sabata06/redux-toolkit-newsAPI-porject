868d11c6cc97fd3fcfb9371b8e2ea2f402b6f95e
sabata06 <m.karaarslan06@outlook.com>
2024-06-01 23:46:03 +0300

    login-reducers

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import AppRouter from "./router/AppRouter"
+import { Provider } from "react-redux";
+import AppRouter from "./router/AppRouter";
+import { store } from "./app/store";
 
 function App() {
   return (
-    <div>
+    <Provider store={store}>
       <AppRouter />
-    </div>
-  )
+    </Provider>
+  );
 }
 
-export default App
+export default App;
